0b45d90d07e36a1647d577875f1c86195a2b6bd9
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-10-17 20:13:05 -0700

    set up skeleton for blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,8 +3,9 @@ import React from "react";
 const Blog = () => (
   <div>
     <h1>Blog</h1>
-    <p>Welcome to my blog!</p>
+    <p>Welcome to my blog! This is oscarmmgg1234</p>
   </div>
 );
 
 export default Blog;
+
