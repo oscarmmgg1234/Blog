029ad231513660e808d4bf3545a27ac08cc8b1a4
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-10-17 21:31:24 -0700

    pushing to pages

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,6 +3,7 @@ import React from "react";
 const Blog = () => (
   <div>
     <h1>Blog</h1>
+    <h2>Content goes here...</h2>
     <p>Welcome to my blog! This is oscarmmgg1234 and this is my blog</p>
   </div>
 );
